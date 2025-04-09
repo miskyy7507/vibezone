d544656f696da824ce3d9f6be7a53bbf36e896f5
miskyy7507 <misiulek0w0@gmail.com>
2025-04-09 09:42:37 +0200

    add session middleware

--- server/src/config.ts
+++ server/src/config.ts
@@ -4,5 +4,5 @@ import crypto from "crypto";
 export const config = {
     serverPort: (process.env.PORT && parseInt(process.env.PORT)) || 5000,
     databaseUri: process.env.MONGO_URI || "",
-    jwtSecret: process.env.JWT_SECRET || crypto.randomBytes(64).toString("hex")
+    sessionSecret: process.env.SESSION_SECRET || crypto.randomBytes(64).toString("hex")
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 import { App } from "./app.js";
 import { config } from "./config.js";
@@ -20,24 +22,48 @@ try {
         console.error("MongoDB connection error:", error);
     });
 } catch (error) {
-    console.error("There was an error trying to connect to the database.\nError message:");
+    console.error(
+        "There was an error trying to connect to the database.\nError message:"
+    );
     console.error(error);
     console.error("Exiting.");
     await mongoose.connection.destroy();
     process.exit(1);
 }
 
+const sessionOptions: session.SessionOptions = {
+    cookie: {
+        path: "/",
+        httpOnly: true,
+        secure: false,
+        // maxAge: 14 * 24 * 60 * 60 * 1000, // 14 days in milliseconds
+        maxAge: 30 * 60 * 1000, // 30 minutes for testing
+        sameSite: "strict",
+    },
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        client: mongoose.connection.getClient(),
+        ttl: 30 * 60, // 30 minutes for testing
+    }),
+};
+
+/* TODO: do the proper configuration for production
+ * when express.js is behind reverse proxy like nginx
+ */
+// if (app.get('env') === 'production') {
+//     app.set('trust proxy', 1) // trust first proxy
+//     sess.cookie.secure = true // serve secure cookies
+// }
+
 // API server creation
 const app = new App(
-    [logger, express.json()],
-    [
-        new PostController(),
-        new AuthController()
-    ]
+    [logger, session(sessionOptions), express.json()],
+    [new PostController(), new AuthController()]
 );
 
 app.listen(config.serverPort);
-
 
 process.on("SIGINT", async () => {
     await mongoose.connection.destroy();
