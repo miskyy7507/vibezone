2227be1a1ff27f35979471ad89ce330f0a3e3d07
miskyy7507 <misiulek0w0@gmail.com>
2025-04-12 22:00:29 +0200

    change warning message

--- server/src/config.ts
+++ server/src/config.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import crypto from "crypto";
 
 function warnNotSetSecret() {
-    console.warn("SESSION_SECRET is not set in .env file. \
-        \nGenerated a new secret in the fly, invalidating all previous sessions.");
+    console.warn("SESSION_SECRET variable has not been set in .env file. Generating a random one for this run.");
+    console.warn("Please set this variable to avoid invalidating session tokens every time a server is reset.")
     return crypto.randomBytes(64).toString("hex");
 }
 
