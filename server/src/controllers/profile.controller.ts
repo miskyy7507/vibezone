f3e55589887208486ab2e491916dd301b0c40d83
miskyy7507 <misiulek0w0@gmail.com>
2025-04-12 02:08:02 +0200

    implement profile settings update

--- server/src/controllers/profile.controller.ts
+++ server/src/controllers/profile.controller.ts
@@ -1,7 +1,9 @@
 import { RequestHandler, Router } from "express";
+import { Types } from "mongoose";
+import { z } from "zod";
+
 import { Controller } from "../interfaces/controller.interface.js";
 import { ProfileService } from "../services/profile.service.js";
-import { Types } from "mongoose";
 
 export class ProfileController implements Controller {
     public path = "/api/profile";
@@ -12,10 +14,10 @@ export class ProfileController implements Controller {
     constructor() {
         this.router.get("/:id", this.getProfile);
 
+        this.router.patch("/update", this.updateProfile);
+
         // this.router.post("/pfp", this.updatePfp);
         // this.router.delete("/pfp", this.removePfp);
-
-        // this.router.patch("/settings", this.updateSettings);
     }
 
     private getProfile: RequestHandler = async (request, response, next) => {
@@ -28,11 +30,68 @@ export class ProfileController implements Controller {
         }
 
         try {
-            const profile = await this.profileService.getById(new Types.ObjectId(id));
+            const profile = await this.profileService.getById(
+                new Types.ObjectId(id)
+            );
             if (!profile) {
                 return response.status(404).json({ error: "Not found" });
             }
             return response.status(200).json(profile);
+        } catch (error) {
+            console.error("Unknown error:", error);
+            return response
+                .status(500)
+                .json({ error: "Internal server error" });
+        }
+    };
+
+    private updateProfile: RequestHandler = async (
+        request,
+        response,
+        next
+    ) => {
+        if (!request.session.profileId) {
+            return response.status(401).json({ error: "Unauthorized" });
+        }
+
+        const update = request.body;
+
+        let validatedUpdate;
+        try {
+            validatedUpdate = await z
+                .object({
+                    displayName: z
+                        .string()
+                        .nonempty("Display name cannot be empty")
+                        .max(32, "Display name too long")
+                        .optional(),
+                    aboutDesc: z
+                        .string()
+                        .max(150, "Description too long")
+                        .optional()
+                        .nullable(),
+                })
+                .parseAsync(update);
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                console.error("Validation error:", error);
+                return response.status(400).json({
+                    error: error.errors[0].message,
+                    item: error.errors[0].path.at(-1),
+                });
+            }
+            console.error("Unknown error:", error);
+            return response
+                .status(500)
+                .json({ error: "Internal server error" });
+        }
+
+        try {
+            const result = await this.profileService.updateProfile(
+                new Types.ObjectId(request.session.profileId),
+                validatedUpdate
+            );
+            return response.status(200).json(result);
         } catch (error) {
             console.error("Unknown error:", error);
             return response
--- server/src/services/profile.service.ts
+++ server/src/services/profile.service.ts
@@ -2,13 +2,38 @@ import { Types } from "mongoose";
 import { ProfileModel } from "../models/profile.model.js";
 import { IProfile } from "../interfaces/profile.interface.js";
 
+type Nullable<T> = {
+    [P in keyof T]: T[P] | null;
+};
+
 export class ProfileService {
     public async createProfile(username: string, displayName: string) {
-        const dataModel = new ProfileModel<IProfile>({username, displayName});
+        const dataModel = new ProfileModel<IProfile>({ username, displayName });
         return dataModel.save();
     }
 
     public async getById(id: Types.ObjectId) {
         return await ProfileModel.findById(id);
+    }
+
+    public async updateProfile(
+        id: Types.ObjectId,
+        updateData: Partial<Nullable<IProfile>>
+    ) {
+        const setFields: Record<string, any> = {};
+        const unsetFields: Record<string, any> = {};
+
+        for (const key in updateData) {
+            const value = (updateData as Record<string, any>)[key]
+            if (value !== null) {
+                setFields[key] = value;
+            } else {
+                unsetFields[key] = "";
+            }
+        }
+
+        const update: Record<"$set" | "$unset", Record<string, any>> = {"$set": setFields, "$unset": unsetFields};
+
+        return await ProfileModel.findByIdAndUpdate(id, update, { new: true });
     }
 }
