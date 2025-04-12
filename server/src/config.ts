6206716cd921530577dba3e26f2279720962bcd9
miskyy7507 <misiulek0w0@gmail.com>
2025-04-12 02:07:15 +0200

    add warning when running server without session secret set in env

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,8 +1,14 @@
 import "dotenv/config";
 import crypto from "crypto";
 
+function warnNotSetSecret() {
+    console.warn("SESSION_SECRET is not set in .env file. \
+        \nGenerated a new secret in the fly, invalidating all previous sessions.");
+    return crypto.randomBytes(64).toString("hex");
+}
+
 export const config = {
     serverPort: (process.env.PORT && parseInt(process.env.PORT)) || 5000,
     databaseUri: process.env.MONGO_URI || "",
-    sessionSecret: process.env.SESSION_SECRET || crypto.randomBytes(64).toString("hex")
+    sessionSecret: process.env.SESSION_SECRET || warnNotSetSecret()
 }
