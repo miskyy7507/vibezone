e3e4d9bc7379f61973f16ff14b9fc737b0595c8b
miskyy7507 <misiulek0w0@gmail.com>
2025-04-29 12:32:44 +0200

    move navbar components to components directory

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router";
 
-import { Navbar } from "./Navbar";
+import { Navbar } from "./components/Navbar";
 import { Home } from "./pages/Home";
 import { Users } from "./pages/Users";
 
