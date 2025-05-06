2a9c738a653ab5cc3b987ca885d75ec0ce272e7b
miskyy7507 <misiulek0w0@gmail.com>
2025-05-06 22:47:13 +0200

    update tooling config

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
