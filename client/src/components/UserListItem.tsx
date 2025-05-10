2b912f3e2cc5fbb89c8e8abc0735ef9f4be99aba
miskyy7507 <misiulek0w0@gmail.com>
2025-05-10 22:59:47 +0200

    fetch user list from server

--- client/src/components/UserListItem.tsx
+++ client/src/components/UserListItem.tsx
@@ -5,16 +5,10 @@ import type { User } from "../interfaces/user.interface";
 export function UserListItem({ user }: { user: User }) {
     return (
         <div className="flex flex-row p-4 border-b border-gray-200 max-w-2xl w-full">
-            <ProfilePicture
-                uri={user.profilePictureUri}
-                username={user.displayName ?? user.username}
-            />
+            <ProfilePicture user={user} />
             <div className="flex flex-col ml-2 ">
                 <div className="flex flex-row space-x-2 items-center">
-                    <UserNamesDisplay
-                        username={user.username}
-                        displayName={user.displayName}
-                    />
+                    <UserNamesDisplay user={user} />
                 </div>
                 {user.aboutDesc && (
                     <span className="text-gray-400">
--- client/src/pages/Users.tsx
+++ client/src/pages/Users.tsx
@@ -1,17 +1,45 @@
+import { useState, useEffect } from "react";
+
 import { UserListItem } from "../components/UserListItem";
+import { Spinner } from "../components/Spinner";
 
-import { placeholderUsers } from "../placeholderUsers";
-
+import type { User } from "../interfaces/user.interface";
 
 export function Users() {
+    const [users, setUsers] = useState<User[] | null>(null);
+
+    useEffect(() => { void (async () => {
+        try {
+            const response = await fetch("http://localhost:6660/api/profile/all", {
+                method: "GET",
+                credentials: "include",
+            });
+            if (response.status !== 200) {
+                console.error(await response.text());
+                alert(
+                    `An unexpected error occured when trying to get list of users. The server responsed with code: ${response.status.toString()}`
+                );
+            }
+            const data = (await response.json()) as User[];
+            setUsers(data);
+        } catch (error) {
+            if (error instanceof TypeError) {
+                console.error("Fetch failed.", error);
+                alert(`Something went wrong: ${error.message}`);
+            } else {
+                throw error;
+            }
+        }
+    })();
+    }, []);
 
     return (
         <div className="flex flex-col items-center">
-            {
-                placeholderUsers.map((user) => (
-                    <UserListItem user={user} key={user._id} />
-                ))
-            }
+            {users ? (
+                users.map((user) => <UserListItem user={user} key={user._id} />)
+            ) : (
+                <Spinner size="large" theme="dark" />
+            )}
         </div>
     );
 }
