644782bf950c23784bd23cfbed432b288907af6c
miskyy7507 <misiulek0w0@gmail.com>
2025-05-10 12:48:33 +0200

    make ProfilePicture component resizable

--- client/src/components/ProfilePicture.tsx
+++ client/src/components/ProfilePicture.tsx
@@ -1,13 +1,24 @@
 export function ProfilePicture({
     uri,
     username,
+    size = "normal"
 }: {
     uri?: string;
     username: string;
+    size?: "small" | "normal"
 }) {
     const altText = uri ? `Profile picture of user ${username}` : "Default profile picture";
+
+    const sizeMap = {
+        small: 32,
+        normal: 48,
+    };
+
+    const sizePx = `${sizeMap[size].toString()}px`;
+
     return (
         <img
+            style={{ width: sizePx, height: sizePx }}
             className="size-12 rounded-full object-cover"
             src={uri ?? "pfp_placeholder.svg"}
             alt={altText}
