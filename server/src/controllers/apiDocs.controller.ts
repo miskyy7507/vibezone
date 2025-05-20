3e76e2c17087e67d17ca8c28d422f94ac8fc45f7
miskyy7507 <misiulek0w0@gmail.com>
2025-05-20 10:54:09 +0200

    load swagger documentation directly from the yaml file without json conversion

--- server/src/controllers/apiDocs.controller.ts
+++ server/src/controllers/apiDocs.controller.ts
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from '../api_documentation.json' with { type: 'json' };
+
+import yaml from "js-yaml";
+import fs from "node:fs";
+
 import type { Controller } from "../interfaces/controller.interface.js";
+
 
 export class ApiDocsController implements Controller {
     public path = "/";
     public router = Router();
 
     constructor() {
+        const swaggerDocument = yaml.load(fs.readFileSync("swagger.yaml", "utf8"));
+        if (!swaggerDocument || typeof swaggerDocument !== "object") {
+            throw new Error("Could not load swagger.yaml file in this server project directory.");
+        }
         this.router.use("/", swaggerUi.serve);
         this.router.get("/", swaggerUi.setup(swaggerDocument));
     }
