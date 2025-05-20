ac8255e4c07dc0656385da380f77907dab14dcde
miskyy7507 <misiulek0w0@gmail.com>
2025-05-20 14:13:43 +0200

    fix returning role from api

--- server/src/controllers/profile.controller.ts
+++ server/src/controllers/profile.controller.ts
@@ -64,7 +64,7 @@ export class ProfileController implements Controller {
             if (!profile) {
                 return response.status(404).json({ error: "Not found" });
             }
-            return response.status(200).json({ ...profile, role: request.session.role });
+            return response.status(200).json(profile);
         } catch (error) {
             next(error);
             return;
@@ -80,7 +80,7 @@ export class ProfileController implements Controller {
             const profile = await this.profileService.getById(
                 new Types.ObjectId(request.session.profileId)
             );
-            return response.status(200).json(profile);
+            return response.status(200).json({ ...profile, role: request.session.role });
         } catch (error) {
             next(error);
             return;
